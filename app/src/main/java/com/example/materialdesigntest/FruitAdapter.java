49660941c29a4f72730ad12abdcf5697605af1eb
任富强Home <renatqiang@163.coom>
2019-06-23 13:54:24 +0800

    public ViewHolder onCreateViewHolder (@NonNull ViewGroup parent, int viewType)函数 返回值错误，已修改

--- app/src/main/java/com/example/materialdesigntest/FruitAdapter.java
+++ app/src/main/java/com/example/materialdesigntest/FruitAdapter.java
@@ -46,14 +46,14 @@ public class FruitAdapter extends RecyclerView.Adapter <FruitAdapter.ViewHolder>
             @Override
             public void onClick(View v) {
                 int position = holder.getAdapterPosition();
-                Fruit fruit =mFruitList.get(0);
+                Fruit fruit =mFruitList.get(position);
                 Intent intent = new Intent(mContext,FruitActivity.class);
                 intent.putExtra(FruitActivity.FRUIT_NAME,fruit.getName());
                 intent.putExtra(FruitActivity.FRUIT_IMAGE_ID,fruit.getImageId());
                 mContext.startActivity(intent);
             }
         });
-        return new ViewHolder(view);
+        return holder;
     }
 
     @Override
